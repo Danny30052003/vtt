47ae401d189c06c096edb8aae71dff764a55c632
Danny30052003 <45289938+Danny30052003@users.noreply.github.com>
2018-11-23 22:57:05 +0700

    Add files via upload

---
+++ VTT chatible/custom/lang.js
@@ -0,0 +1,44 @@
+module.exports = {
+	BAO_TRI : "[VTT BOT] VTT Chatible hiện đang bảo trì!",
+	FIRST_COME : '[VTT BOT] Chào mừng bạn đến với VTT Chatible. Chúc bạn có 1 thời gian vui vẻ tại nơi đây.\n\nẤn trợ giúp (hoặc gửi trogiup) để xem thêm.',
+	HUONG_DAN : "[VTT BOT] Hãy gửi batdau hoặc bấm vào nút để bắt đầu Chat.",
+	BATDAU_OKAY : "[VTT BOT] Đang tìm...",
+	BATDAU_WARN_GENDER : "[VTT BOT] Lưu ý: Bạn không chọn giới tính. Có thể bạn sẽ phải đợi lâu hơn.",
+	BATDAU_ERR_ALREADY : "[VTT BOT] Bạn đang trong 1 cuộc nói chuyện,hãy gửi ketthuc để kết thúc trước khi bắt đầu nhé!",
+	WAITING : "[VTT BOT] Đang tìm...",
+	START_CHAT : "[VTT BOT] Đã kết nối bạn với người dùng khác!\nNếu muốn kết thúc hãy gửi ketthuc",
+	END_CHAT : "[VTT BOT] Cuộc trò chuyện kết thúc!\nGửi batdau hoặc bấm vào nút để kết nối với người dùng khác!",
+	END_CHAT_PARTNER : "[VTT BOT] Bạn ý đã ngắt kết nối :(\nGửi batdau hoặc bấm vào nút để tìm bạn chat.",
+	END_CHAT_FORCE : "[VTT BOT] Hiện tại không có ai đang online cả. Bạn hãy thử lại sau nhé :(",
+	ERR_UNKNOWN : "[VTT BOT] Server xảy ra lỗi nhưng không nghiêm trọng lắm\nHãy gửi ketthuc để thoát ra và thử lại",
+	ERR_ATTACHMENT : "[VTT BOT] Lỗi: Chúng mình chưa hỗ trợ gửi dạng dữ liệu này",
+	ATTACHMENT_FILE : "[VTT BOT] Người dùng kia đã gửi 1 tệp tin: ",
+	ATTACHMENT_LINK : "[VTT BOT] Người dùng kia đã gửi 1 đường link: ",
+	ERR_FAKE_MSG : "[VTT BOT] Lỗi: Vui lòng không giả mạo tin nhắn của BOT!",
+	DATABASE_ERR : "[VTT BOT] Lỗi: Không thể kết nối với cơ sở dữ liệu. Hãy báo cho admin!",
+	GENDER_ERR : "[VTT BOT] Lỗi: Giới tính nhập vào không hợp lệ!\n\nẤn trợ giúp (hoặc gửi trogiup) để xem thêm.",
+	GENDER_WRITE_OK : "[VTT BOT] Bạn đã chọn giới tính mong muốn tìm được là: ",
+	GENDER_WRITE_WARN : "\n\nLưu ý: Tùy chọn này chỉ có tác dụng với PHẦN LỚN các cuộc nói chuyện.",
+	GENDER_ARR : ['không quan tâm', 'nam', 'nữ'],
+	HELP_TXT : "[VTT BOT] Danh sách các lệnh:\n" +
+				"- batdau: Bắt đầu tìm bạn chat\n" +
+				"- ketthuc: Kết thúc chat\n" +
+				"- trogiup: Xem trợ giúp\n" +
+		    	"\nCác lệnh có thể dùng khi đang không chat:\n" +,
+	KEYWORD_BATDAU : 'batdau',
+	KEYWORD_KETTHUC : 'ketthuc',
+	KEYWORD_HELP : 'trogiup',
+	KEYWORD_MAINTAIN : 'maintain',
+	ERR_SERVER : '[VTT BOT] Có lỗi xảy ra với chatbot. Tin nhắn của bạn chưa được xử lý. Bạn hãy thử lại sau 1 phút nữa nhé.',
+	ERR_200 : '[VTT BOT] Bạn chat không thể nhận tin nhắn do đã xóa inbox hoặc block page.',
+	ERR_230 : '[VTT BOT] Bạn chat không thể nhận tin nhắn do 2 bạn không nói chuyện gì trong vòng 24h. Gửi ketthuc để kết thúc chat.',
+	ERR_TOO_LONG : '[VTT BOT] Lỗi: Tin nhắn quá dài (nhiều hơn 640 ký tự)!'
+};
+// Những đoạn mã bị vô hiệu hóa
+  //  KEYWORD_CAT : 'meow',
+  //  KEYWORD_DOG : 'gauw',
+//"- meow: Xem ảnh mèo\n" +
+//"- gauw: Xem ảnh cún\n" +
+//"- timnu: Tìm nữ chat cùng\n" +
+//"- timnam: Tìm nam chat cùng",
+//	KEYWORD_GENDER : 'tim',
